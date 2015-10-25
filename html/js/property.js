b06e4a2ef4a1f3ffba3547401ee08a7b29de114d
Sergey Dobrynin <wbearfromru@gmail.com>
2015-10-25 18:10:57 +0100

    Keep open tabs on property page

--- html/js/property.js
+++ html/js/property.js
@@ -11,4 +11,16 @@ $(document).on('click', 'tr.tenant', function(e) {
 
 $(document).on('click', 'tr.issue', function(e) {
     location.href = 'problem.html';
-})
+});
+
+$(document).on('shown.bs.tab','a[data-toggle="tab"]', function (e) {
+	var url = $(this)[0].href;
+	var hash = url.substring(url.indexOf('#')+1);
+	location.hash = hash;
+});
+
+var tabToOpen = location.hash;
+if (tabToOpen) {
+	$('.nav-tabs a[href="'+tabToOpen+'"]').tab('show');	
+}
+
