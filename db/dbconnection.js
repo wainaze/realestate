6707f1bbfde33b4a0469ad05223035ada65cf357
Sergey Dobrynin <wbearfromru@gmail.com>
2015-11-15 02:49:24 +0100

    db integration

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -3,13 +3,11 @@ var monk = require('monk');
 
 var host = process.env.OPENSHIFT_MONGODB_DB_HOST ? process.env.OPENSHIFT_MONGODB_DB_HOST : '127.0.0.1';
 var port = process.env.OPENSHIFT_MONGODB_DB_PORT ? process.env.OPENSHIFT_MONGODB_DB_PORT : '27017';
-var username = process.env.OPENSHIFT_MONGODB_DB_USERNAME ? process.env.OPENSHIFT_MONGODB_DB_USERNAME : '';
-var password = process.env.OPENSHIFT_MONGODB_DB_PASSWORD ? process.env.OPENSHIFT_MONGODB_DB_PASSWORD : '';
 
-console.log('Connect to DB: ' + JSON.stringify({host: host, port: port, username : username, password: password}));
+console.log('Connect to DB: ' + JSON.stringify({host: host, port: port}));
 var db = monk(host + ':' + port + '/web', {
-  username : username,
-  password : password
+  username : 'azurent',
+  password : 'ZjdtsdfYsdof'
 });
 
 exports.db = db;
