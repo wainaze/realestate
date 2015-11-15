c9e6dbac84843b399dd02e82ab9f078aee8c9ca8
Sergey Dobrynin <wbearfromru@gmail.com>
2015-11-15 02:07:33 +0100

    db integration

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,12 +1,12 @@
 var mongo = require('mongodb');
 var monk = require('monk');
 
-var host = process.env.OPENSHIFT_MONGODB_DB_HOST? process.env.OPENSHIFT_MONGODB_DB_HOST : '127.0.0.1';
-var port = process.env.OPENSHIFT_MONGODB_DB_PORT? process.env.OPENSHIFT_MONGODB_DB_PORT : '27017';
-var username = process.env.OPENSHIFT_MONGODB_DB_USERNAME? process.env.OPENSHIFT_MONGODB_DB_USERNAME : '';
-var password = process.env.OPENSHIFT_MONGODB_DB_PASSWORD? process.env.OPENSHIFT_MONGODB_DB_PASSWORD : '';
+var host = process.env.OPENSHIFT_MONGODB_DB_HOST ? process.env.OPENSHIFT_MONGODB_DB_HOST : '127.0.0.1';
+var port = process.env.OPENSHIFT_MONGODB_DB_PORT ? process.env.OPENSHIFT_MONGODB_DB_PORT : '27017';
+var username = process.env.OPENSHIFT_MONGODB_DB_USERNAME ? process.env.OPENSHIFT_MONGODB_DB_USERNAME : '';
+var password = process.env.OPENSHIFT_MONGODB_DB_PASSWORD ? process.env.OPENSHIFT_MONGODB_DB_PASSWORD : '';
 
-
+console.log('Connect to DB: ' + JSON.stringify({host: host, port: port, username : username, password: password}));
 var db = monk(host + ':' + port + '/web', {
   username : username,
   password : password
